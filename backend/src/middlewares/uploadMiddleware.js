0eeb811ec5757282f9b2dc6a17a51e1e3b855752
Angel Ibarra <ibarragc.iska@gmail.com>
2025-03-26 15:39:14 -0600

    Corrección en la configuración de cloudinary

--- backend/src/middlewares/uploadMiddleware.js
+++ backend/src/middlewares/uploadMiddleware.js
@@ -3,7 +3,7 @@ const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const cloudinary = require('cloudinary').v2;
 require("dotenv").config();
 
-cloudinary.v2.config({
+cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
